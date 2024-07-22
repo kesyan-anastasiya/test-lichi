5cb5fe1e497e9c22c8e88c4b99c69993325d9c1d
kesyan-anastasiya <kesyan_anastasiya@mail.ru>
2024-07-22 15:38:09 +0300

    feat: added buttons on the articles page

--- src/pages/articles/index.js
+++ src/pages/articles/index.js
@@ -1,7 +1,21 @@
-export default function Article() {
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+export default function Articles() {
+  const router = useRouter();
+
+  const linkClickHandler = () => {
+    router.push("/");
+  };
+
   return (
     <>
+      <Head>
+        <title>Список статей</title>
+      </Head>
       <h1>Список статей</h1>
+      <button onClick={linkClickHandler}>На главную</button>
+      <button onClick={() => router.push(`/articles/${id}`)}>Подробнее</button>
     </>
   );
 }
